0beab69e37d2b59c8ab99ae3933abfecf4995178
lee-lennox <lennoxkomane@gmail.com>
2026-05-06 10:20:02 +0200

    category controller

--- Backend/src/main/java/za/ac/youthVend/controller/CategoryController.java
+++ Backend/src/main/java/za/ac/youthVend/controller/CategoryController.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:3000"})
 public class CategoryController {
 
     private final CategoryService categoryService;
