85a68fce404b3f865423c4a4cdc36bd63cf6e8a5
lee-lennox <lennoxkomane@gmail.com>
2026-05-06 10:20:41 +0200

    address controller

--- Backend/src/main/java/za/ac/youthVend/controller/AddressController.java
+++ Backend/src/main/java/za/ac/youthVend/controller/AddressController.java
@@ -16,7 +16,6 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/addresses")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:3000"})
 public class AddressController {
 
     private final AddressService addressService;
